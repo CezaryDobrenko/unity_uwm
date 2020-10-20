6ec517cddcaf9ae5ebc08b92961e752b37a2f23d
CezaryDobrenko <bonzohaker742@gmail.com>
2020-10-20 12:32:27 +0200

    finished lab03

---
+++ Laborki/Assets/Scripts/lab03/Zadanie2.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Zadanie2 : MonoBehaviour
+{
+
+    public float speed;
+    int direction = 1;
+
+    void Update()
+    {
+        //Modern problems require modern solutions ¯\_(ツ)_/¯
+
+        if (transform.position.x > 10)
+            direction = -1;
+        else if (transform.position.x < 0)
+            direction = 1;
+
+        transform.Translate(Vector3.right * direction * speed * Time.deltaTime);
+
+    }
+
+
+}
---
+++ Laborki/Assets/Scripts/lab03/Zadanie3.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Zadanie3 : MonoBehaviour
+{
+
+    public float speed;
+    public Vector3[] points;
+    private int nextPointIndex = 0;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        //K.I.S.S ¯\_(ツ)_/¯
+
+        if ((Vector3)transform.position == points[nextPointIndex])
+        {
+            nextPointIndex = (nextPointIndex + 1) % points.Length;
+            transform.Rotate(0,90,0);
+        }
+
+        transform.position = Vector3.MoveTowards(transform.position, points[nextPointIndex], speed * Time.deltaTime);
+    }
+}
