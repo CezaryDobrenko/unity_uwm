52c84946dd42fd2c18245be95affe7bc5d640421
CezaryDobrenko <bonzohaker742@gmail.com>
2020-11-01 20:23:10 +0100

    lab 04 ex.4,5

---
+++ Laborki/Assets/Scripts/lab04/zadanie4.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class zadanie4 : MonoBehaviour
+{
+    public Transform player;
+
+    public float sensitivity = 200f;
+    float horizontalRotation = 0;
+    float verticalRotation = 0;
+
+    void Start()
+    {
+        // zablokowanie kursora na środku ekranu, oraz ukrycie kursora
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        horizontalRotation += Input.GetAxis("Mouse X") * sensitivity * Time.deltaTime;
+        verticalRotation -= Input.GetAxis("Mouse Y") * sensitivity * Time.deltaTime;
+
+        verticalRotation = Mathf.Clamp(verticalRotation, -90, 90);
+        player.transform.localRotation = Quaternion.Euler(verticalRotation, horizontalRotation, 0);
+
+
+    }
+}
