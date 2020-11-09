3b54560b4bcac2461840ea497c1276c554ce164f
CezaryDobrenko <bonzohaker742@gmail.com>
2020-11-09 23:26:00 +0100

    Lab 05 finished

---
+++ Laborki/Assets/Scripts/lab05/zaadanie3.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class zaadanie3 : MonoBehaviour
+{
+
+    public float speed;
+    public Vector3[] points;
+    private int nextPointIndex = 0;
+    bool direction = true;
+    int test = 1;
+
+    void Update()
+    {
+        //Make america great again (404 trump not found)
+
+        if ((Vector3)transform.position == points[nextPointIndex])
+        {
+            nextPointIndex = (nextPointIndex + test);
+            //Reverse direction of platform if index 0 or index pionts.length
+            if (nextPointIndex == points.Length-1 || nextPointIndex == 0)
+                test = test * -1;
+        }
+
+        transform.position = Vector3.MoveTowards(transform.position, points[nextPointIndex], speed * Time.deltaTime);
+    }
+}
---
+++ Laborki/Assets/Scripts/lab05/zadanie1.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class zadanie1 : MonoBehaviour
+{
+    public float elevatorSpeed = 2f;
+    private bool isRunning = false;
+    public float distance = 6.6f;
+    private bool isAway = false;
+    private float backward;
+    private float forward;
+
+    void Start()
+    {
+        forward = transform.position.z + distance;
+        backward = transform.position.z;
+    }
+
+    void Update()
+    {
+        if (transform.position.z >= forward && isAway == false)
+        {
+            isAway = true;
+            elevatorSpeed = -elevatorSpeed;
+        }
+        if (transform.position.z < backward && isAway == true)
+        {
+            isRunning = false;
+            isAway = false;
+            elevatorSpeed = -elevatorSpeed;
+        }
+        if (isRunning)
+        {
+            Vector3 move = transform.forward * elevatorSpeed * Time.deltaTime;
+            transform.Translate(move);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            isRunning = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("Player zszedł z windy.");
+        }
+    }
+
+}
